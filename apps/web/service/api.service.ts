de5fd951ca2749945f8d878ad785e3a709c38848
otongs <muhammadkharismam@gmail.com>
2025-05-23 12:55:57 +0800

    fix: remove any change number in parameter id

--- apps/web/service/api.service.ts
+++ apps/web/service/api.service.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export class ApiService {
   get = async(baseUrl: string, endpoint: string, header: object) => await axios.get(`${baseUrl}/${endpoint}`, header)
-  getById = async(baseUrl: string, endpoint: string, header: object, id: any) => await axios.get(`${baseUrl}/${endpoint}/${id}`, header)
+  getById = async(baseUrl: string, endpoint: string, header: object, id: number) => await axios.get(`${baseUrl}/${endpoint}/${id}`, header)
   post = async(baseUrl: string, endpoint: string, header: object, data: object) => await axios.post(`${baseUrl}/${endpoint}`, data, header)
-  put = async(baseUrl: string, endpoint: string, header: object, data: object, id: any) => await axios.put(`${baseUrl}/${endpoint}/${id}`, data, header)
+  put = async(baseUrl: string, endpoint: string, header: object, data: object, id: number) => await axios.put(`${baseUrl}/${endpoint}/${id}`, data, header)
   delete = async(baseUrl: string, endpoint: string, header: object, id: number) => await axios.delete(`${baseUrl}/${endpoint}/${id}`, header)
 }
