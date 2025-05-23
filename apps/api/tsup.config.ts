8c13906d1fb4a1bd1ed10f91004eb5846a8b4420
otongs <muhammadkharismam@gmail.com>
2025-05-23 11:29:21 +0800

    feat: complete setting eslint

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['index.ts'],
-  format: ['cjs'],
-  target: 'node20',
-  outDir: 'dist',
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
   sourcemap: true,
   clean: true,
+  target: 'node14',
 });
