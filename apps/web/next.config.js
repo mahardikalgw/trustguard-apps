f0180b15ab20918386f7780b2eb89fbafa2119c2
otongs <muhammadkharismam@gmail.com>
2025-05-23 16:12:38 +0800

    feat: adding shadcn ui ext

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, 'components'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@styles': path.resolve(__dirname, 'styles'),
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
